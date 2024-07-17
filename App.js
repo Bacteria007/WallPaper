1420b433217d6a33eb86f886f7b2e9e8ea41ed75
Bacteria007 <123715803+Bacteria007@users.noreply.github.com>
2024-07-17 23:45:01 +0500

    initial commit

--- App.js
+++ App.js
@@ -3,18 +3,9 @@ import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ProxyScreen from './src/practiceScreens/ProxyScreen';
-import Rotationscreen from './src/practiceScreens/Rotationscreen';
-import Bluetooth from './src/practiceScreens/Bluetooth';
-import Accelero from './src/practiceScreens/Accelero';
-import Demo from './src/practiceScreens/Demo';
 import Welcome from '././src/screens/Welcome';
 import Home from '././src/screens/Home';
-import {
-  BottomSheetModal,
-  BottomSheetView,
-  BottomSheetModalProvider,
-} from '@gorhom/bottom-sheet';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import ImageDetail from "./src/screens/ImageDetail";
 
@@ -27,7 +18,7 @@ const App = () => {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BottomSheetModalProvider>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false,animation:'slide_from_right' }}>
+          <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>
             <Stack.Screen name="Welcome" component={Welcome} />
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="ImageDetail" component={ImageDetail} />
